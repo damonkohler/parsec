4b3d2314dd1c84a6d213f1abb377d4eca0e1b6d4
Lorenz Moesenlechner <moesenle@google.com>
2011-10-10 17:32:18 +0200

    Added tf support to odom_relay node

--- parsec_odometry_relay/src/odometry_relay.py
+++ parsec_odometry_relay/src/odometry_relay.py
@@ -7,8 +7,11 @@ import rospy
 
 from parsec_msgs.msg import Odometry as ParsecOdometry
 from nav_msgs.msg import Odometry
+from tf.msg import tfMessage
+from geometry_msgs.msg import TransformStamped
 
 odom_publisher = rospy.Publisher('odom', Odometry)
+tf_publisher = rospy.Publisher('/tf', tfMessage)
 
 odom = Odometry()
 odom.child_frame_id = 'base_link'
@@ -23,6 +26,14 @@ def relay(data):
   odom.twist.twist.linear.y = data.linear_y
   odom.twist.twist.angular.z = data.angular_z
   odom_publisher.publish(odom)
+
+  transform = TransformStamped()
+  transform.header = data.header
+  transform.child_frame_id = odom.child_frame_id
+  transform.transform.translation = odom.pose.pose.position
+  transform.transform.rotation = odom.pose.pose.orientation
+  tf_publisher.publish(tfMessage(transforms=[transform]))
+
 
 def listener():
   rospy.init_node('parsec_odometry_relay')
