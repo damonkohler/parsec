43f4b56caf23ca1c3e3d347ce565be9f237dd9e3
Lorenz Moesenlechner <moesenle@google.com>
2011-11-10 11:16:00 +0100

    Fixed author name in tf_odometry_relay

--- tf_odometry_relay/src/tf_odometry_relay.cpp
+++ tf_odometry_relay/src/tf_odometry_relay.cpp
@@ -1,5 +1,5 @@
 // Copyright 2011 Google Inc.
-// Author: whess@google.com (Wolfgang Hess)
+// Author: moesenle@google.com (Lorenz Moesenlechner)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,8 +12,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
-// Author: moesenle@google.com (Lorenz Moesenlechner)
 
 #include <string>
 
