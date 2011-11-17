378c16c3113fad1e99200d9e48f09d458b7e324c
Lorenz Moesenlechner <moesenle@google.com>
2011-11-17 19:11:05 +0100

    Improved servo calibration script
    It uses now a least squares method to calculate the min and max angles of the
    servo.

--- parsec_calibration/scripts/calibrate_tilting_laser_servo.py
+++ parsec_calibration/scripts/calibrate_tilting_laser_servo.py
@@ -26,10 +26,13 @@ import roslib; roslib.load_manifest('parsec_calibration')
 import rospy
 
 from parsec_calibration import laser_scans
+from parsec_calibration import find_scan_parameters
 
 import sensor_msgs.msg as sensor_msgs
 import parsec_msgs.msg as parsec_msgs
 
+
+LASER_DISTANCE_FROM_ROTATION_AXIS = 0.033
 
 def _print_results_mean(results, stream):
   count = len(results)
@@ -37,10 +40,12 @@ def _print_results_mean(results, stream):
   low_multiplier_mean = sum(result.low_multiplier for result in results) / count
   high_angle_mean = sum(result.high_angle for result in results) / count
   high_multiplier_mean = sum(result.high_multiplier for result in results) / count
-  stream.write('Low angle mean: %f, %f degrees, multiplier %f\n' % (
+  phase_offset_mean = sum(result.phase_offset for result in results) / count
+  stream.write('Low angle mean: %r, %r degrees, multiplier %r\n' % (
     low_angle_mean, low_angle_mean * 180 / math.pi, low_multiplier_mean))
-  stream.write('High angle mean: %f, %f degrees, multiplier %f\n' % (
+  stream.write('High angle mean: %r, %r degrees, multiplier %r\n' % (
     high_angle_mean, high_angle_mean * 180 / math.pi, high_multiplier_mean))
+  stream.write('Phase offset mean: %r\n' % phase_offset_mean)
 
 
 class CalibrationError(Exception):
@@ -49,18 +54,20 @@ class CalibrationError(Exception):
 
 class CalibrationResult(object):
 
-  def __init__(self, low_angle, low_multiplier, high_angle, high_multiplier):
+  def __init__(self, low_angle, low_multiplier, high_angle, high_multiplier, phase_offset):
     self.low_angle = low_angle
     self.high_angle = high_angle
     self.low_multiplier = low_multiplier
     self.high_multiplier = high_multiplier
+    self.phase_offset = phase_offset
 
   def write(self, stream):
     stream.write(
-      'Calculated low angle %f, %f degrees, multiplier %f.\n' % (
+      'Calculated low angle %r, %r degrees, multiplier %r\n' % (
           self.low_angle, self.low_angle * 180 / math.pi, self.low_multiplier) +
-      'Calculated high angle %f, %f degrees, multiplier %f.\n' % (
-          self.high_angle, self.high_angle * 180 / math.pi, self.high_multiplier))
+      'Calculated high angle %r, %r degrees, multiplier %r\n' % (
+          self.high_angle, self.high_angle * 180 / math.pi, self.high_multiplier) +
+      'Phase offset %r\n' % self.phase_offset)
 
 
 class ServoCalibrationRoutine(object):
@@ -68,11 +75,11 @@ class ServoCalibrationRoutine(object):
 
   def __init__(self, minimum_angle, maximum_angle, tilt_period):
     self._lock = threading.Lock()
-    self._scans = laser_scans.LaserScanQueue()
-    self._reset()
     self._minimum_angle = minimum_angle
     self._maximum_angle = maximum_angle
     self._tilt_period = tilt_period
+    self._tilt_signals = []
+    self._scans = laser_scans.LaserScanQueue()
     self._calibration_results = []
 
   def run(self):
@@ -84,16 +91,17 @@ class ServoCalibrationRoutine(object):
         '~scan', sensor_msgs.LaserScan, self._on_laser_scan)
     self._tilt_profile_publisher.publish(parsec_msgs.LaserTiltProfile(
           min_angle=self._minimum_angle, max_angle=self._maximum_angle,
-          period=self._tilt_period))
+          increasing_duration=self._tilt_period/2,
+          decreasing_duration=self._tilt_period/2))
     rospy.spin()
 
   def _on_tilt_signal(self, signal):
     with self._lock:
-      if signal.signal == parsec_msgs.LaserTiltSignal.ANGLE_DECREASING:
-        # Reached top of the scan range.
-        self._angle_decreasing_stamp = signal.header.stamp
-      elif signal.signal == parsec_msgs.LaserTiltSignal.ANGLE_INCREASING:
-        self._angle_increasing_stamp = signal.header.stamp
+      self._tilt_signals.append(signal)
+      if len(self._tilt_signals) > 3:
+        self._tilt_signals = self._tilt_signals[-4:-1]
+      if self._tilt_signals[0].signal != parsec_msgs.LaserTiltSignal.ANGLE_INCREASING:
+        self._tilt_signals = self._tilt_signals[1:]
 
   def _on_laser_scan(self, scan):
     self._scans.add_scan(scan)
@@ -101,69 +109,47 @@ class ServoCalibrationRoutine(object):
 
   def _calculate_calibration(self):
     with self._lock:
-      if (self._angle_increasing_stamp is None or
-          self._angle_decreasing_stamp is None):
+      if len(self._tilt_signals) != 3:
         raise CalibrationError(
-            'Not enough signals received: increasing stamp: %r; decreasing stamp: %r' %
-            (self._angle_increasing_stamp, self._angle_decreasing_stamp))
-      if (self._scans.get_newest_scan().header.stamp < self._angle_decreasing_stamp or
-          self._scans.get_newest_scan().header.stamp < self._angle_increasing_stamp):
-        raise CalibrationError(
-            'No matching scans received that match the signal time stamps.')
-      low_scan = self._scans.find_scan_at_time(self._angle_increasing_stamp)
-      high_scan = self._scans.find_scan_at_time(self._angle_decreasing_stamp)
-      if low_scan is None or high_scan is None:
-        raise CalibrationError('Signal scans not found: low_scan: %r; high_scan: %r' %
-                               (low_scan, high_scan))
-      if self._angle_increasing_stamp < self._angle_decreasing_stamp:
-        scans = self._scans.get_scans_in_interval(self._angle_increasing_stamp,
-                                                  self._angle_decreasing_stamp)
-      else:
-        scans = self._scans.get_scans_in_interval(self._angle_decreasing_stamp,
-                                                  self._angle_increasing_stamp)
-      low_scan_distance = laser_scans.calculate_laser_scan_range(low_scan)
-      high_scan_distance = laser_scans.calculate_laser_scan_range(high_scan)
-      closest_scan_distance = laser_scans.calculate_laser_scan_range(self._find_closest_scan(scans))
-      if low_scan_distance <= closest_scan_distance or high_scan_distance <= closest_scan_distance:
-        raise CalibrationError(
-            'Scan range measurements insufficient for calibration:'
-            'low scan: %r; middle scan: %r; high scan: %r' %
-            (low_scan_distance, closest_scan_distance, high_scan_distance))
-      low_scan_angle = math.acos(closest_scan_distance / low_scan_distance)
-      high_scan_angle = math.acos(closest_scan_distance / high_scan_distance)
-      result = CalibrationResult(low_scan_angle, abs(low_scan_angle / self._minimum_angle),
-                                 high_scan_angle, abs(high_scan_angle / self._maximum_angle))
-      result.write(sys.stdout)
-      self._calibration_results.append(result)
+            'Invalid number of signals. We need at exactly three signals.')
+      if (self._tilt_signals[0].signal != parsec_msgs.LaserTiltSignal.ANGLE_INCREASING or
+          self._tilt_signals[-1].signal != parsec_msgs.LaserTiltSignal.ANGLE_INCREASING):
+        raise CalibrationError('Invalid signals. The first and last signal need to be ANGLE_INCREASING.')
+      if self._scans.get_newest_scan().header.stamp < self._tilt_signals[-1].header.stamp:
+        raise CalibrationError('Newest scan is older than newest signal.')
+      if self._scans.get_oldest_scan().header.stamp > self._tilt_signals[0].header.stamp:
+        raise CalibrationError('Oldest scan is newer than oldest signal.')
+      start_time = self._scans.find_oldest_scan_after_time(
+          self._tilt_signals[0].header.stamp).header.stamp
+      end_time = self._scans.find_newest_scan_before_time(
+          self._tilt_signals[-1].header.stamp).header.stamp
+      scans = self._scans.get_scans_in_interval(start_time, end_time)
+      scan_parameter_finder = find_scan_parameters.FindScanParameters(
+          sensor_distance_from_rotation_axis=LASER_DISTANCE_FROM_ROTATION_AXIS)
+      parameters = scan_parameter_finder.find_scan_parameters(
+          [scan.ranges[int(len(scan.ranges) / 2)] for scan in scans],
+          (end_time - start_time).to_sec() / self._tilt_period)
+      current_result = CalibrationResult(
+          parameters.low_angle, parameters.low_angle / self._minimum_angle,
+          parameters.high_angle, parameters.high_angle / self._maximum_angle,
+          phase_offset = parameters.phase_offset * self._tilt_period)
+      current_result.write(sys.stdout)
+      self._calibration_results.append(current_result)
+      self._tilt_signals = self._tilt_signals[2:]
       _print_results_mean(self._calibration_results, sys.stdout)
       sys.stdout.write('\n')
-    self._reset()
 
   def _maybe_calculate_calibration(self):
     with self._lock:
-      if (self._angle_increasing_stamp is None or
-          self._angle_decreasing_stamp is None):
+      if len(self._tilt_signals) < 3:
         return
-      if (self._scans.get_newest_scan().header.stamp < self._angle_decreasing_stamp or
-          self._scans.get_newest_scan().header.stamp < self._angle_increasing_stamp):
+      if self._tilt_signals[0].signal != parsec_msgs.LaserTiltSignal.ANGLE_INCREASING:
+        return
+      if self._scans.get_newest_scan().header.stamp < self._tilt_signals[-1].header.stamp:
+        return
+      if self._scans.get_oldest_scan().header.stamp > self._tilt_signals[0].header.stamp:
         return
     self._calculate_calibration()
-
-  def _reset(self):
-    with self._lock:
-      self._scans.clear_scans()
-      self._angle_increasing_stamp = None
-      self._angle_decreasing_stamp = None
-
-  def _find_closest_scan(self, scans):
-    closest_distance = laser_scans.calculate_laser_scan_range(scans[0])
-    closest_scan = scans[0]
-    for scan in scans[1:]:
-      distance = laser_scans.calculate_laser_scan_range(scan)
-      if distance < closest_distance:
-        closest_distance = distance
-        closest_scan = scan
-    return closest_scan
 
 
 def main():
