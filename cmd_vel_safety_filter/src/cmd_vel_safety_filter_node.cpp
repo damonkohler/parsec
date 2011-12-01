8095b7e1c5b55ee5a95263d9b23d8476589ed2c0
Damon Kohler <damonkohler@google.com>
2011-12-01 13:55:30 +0100

    merge

---
+++ cmd_vel_safety_filter/src/cmd_vel_safety_filter_node.cpp
@@ -0,0 +1,26 @@
+// Copyright 2011 Google Inc.
+// Author: moesenle@google.com (Lorenz Moesenlechner)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <ros/ros.h>
+
+#include "cmd_vel_safety_filter/cmd_vel_safety_filter.h"
+
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "cmd_vel_safety_filter");
+  cmd_vel_safety_filter::CmdVelSafetyFilter filter(
+      ros::NodeHandle("~"));
+  ros::spin();
+  return 0;
+}
