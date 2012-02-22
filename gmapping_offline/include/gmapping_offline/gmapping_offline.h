574fdd245da0f61beb9ccdcceff15975d3aaecbb
Charles DuHadway <duhaway@google.com>
2012-02-21 22:10:42 -0800

    refactored gmapping_offline based on latest slam_gmapping

--- gmapping_offline/include/gmapping_offline/gmapping_offline.h
+++ gmapping_offline/include/gmapping_offline/gmapping_offline.h
@@ -1,87 +1,108 @@
 /*
- *  Created on: May 11, 2009
- *      Author: duhadway
+ * gmapping_offline
+ * Copyright (c) 2008, Willow Garage, Inc.
+ *
+ * THE WORK (AS DEFINED BELOW) IS PROVIDED UNDER THE TERMS OF THIS CREATIVE
+ * COMMONS PUBLIC LICENSE ("CCPL" OR "LICENSE"). THE WORK IS PROTECTED BY
+ * COPYRIGHT AND/OR OTHER APPLICABLE LAW. ANY USE OF THE WORK OTHER THAN AS
+ * AUTHORIZED UNDER THIS LICENSE OR COPYRIGHT LAW IS PROHIBITED.
+ * 
+ * BY EXERCISING ANY RIGHTS TO THE WORK PROVIDED HERE, YOU ACCEPT AND AGREE TO
+ * BE BOUND BY THE TERMS OF THIS LICENSE. THE LICENSOR GRANTS YOU THE RIGHTS
+ * CONTAINED HERE IN CONSIDERATION OF YOUR ACCEPTANCE OF SUCH TERMS AND
+ * CONDITIONS.
+ *
  */
 
-#ifndef BUILD_MAP_H_
-#define BUILD_MAP_H_
+/* Author: Brian Gerkey */
 
-#include <queue>
-#include <sensor_msgs/LaserScan.h>
-#include <tf/tf.h>
-#include <tf/tfMessage.h>
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "std_msgs/Float64.h"
+#include "nav_msgs/GetMap.h"
+#include "tf/transform_listener.h"
+#include "tf/transform_broadcaster.h"
+#include "message_filters/subscriber.h"
+#include "tf/message_filter.h"
 
-#include "gridfastslam/gridslamprocessor.h"
-#include "sensor/sensor_base/sensor.h"
+#include "gmapping/gridfastslam/gridslamprocessor.h"
+#include "gmapping/sensor/sensor_base/sensor.h"
 
-namespace build_map
+class GMappingOffline
 {
+  public:
+    GMappingOffline();
+    ~GMappingOffline();
 
-class BuildMap
-{
- public:
-  BuildMap();
-  ~BuildMap();
+    bool processBag(const std::string& bag_file_path);
+    bool saveMap(const std::string& file_name);
+        
+  private:
+    ros::NodeHandle node_;    
+    tf::Transformer tf_;
+    tf::MessageFilter<sensor_msgs::LaserScan>* scan_filter_;
 
-  bool processBag(std::string bag_path);
-  bool saveMap(std::string file);
+    GMapping::GridSlamProcessor* gsp_;
+    GMapping::RangeSensor* gsp_laser_;
+    double gsp_laser_angle_increment_;
+    unsigned int gsp_laser_beam_count_;
+    GMapping::OdometrySensor* gsp_odom_;
 
- private:
-  void tfHandler(tf::tfMessage::ConstPtr m);
+    bool got_first_scan_;
 
-  GMapping::GridSlamProcessor* gsp_;
-  GMapping::RangeSensor* gsp_laser_;
-  GMapping::OdometrySensor* gsp_odom_;
-  GMapping::ScanMatcher matcher_;
-  tf::Transformer tf_;
+    bool got_map_;
+    nav_msgs::GetMap::Response map_;
 
-  bool inverted_laser_;
-  bool first_scan_;
-  int scan_count_;
+    ros::Duration map_update_interval_;
+    tf::Transform map_to_odom_;
+    boost::mutex map_to_odom_mutex_;
+    boost::mutex map_mutex_;
 
-  std::string base_frame_;
-  std::string laser_frame_;
-  std::string odom_frame_;
-  std::string laser_topic_;
+    int laser_count_;
 
-  std::queue<sensor_msgs::LaserScan> scans_;
-  std::queue<sensor_msgs::LaserScan> bad_scans_;
+    std::string base_frame_;
+    std::string laser_frame_;
+    std::string map_frame_;
+    std::string odom_frame_;
+    std::string laser_topic_;
 
-  bool initialize(const sensor_msgs::LaserScan& scan);
-  bool addScan(const sensor_msgs::LaserScan& scan);
-  bool getOdomPose(GMapping::OrientedPoint& gmap_pose, const ros::Time& t);
-  void updateMap(const sensor_msgs::LaserScan& scan);
-
-  // Parameters used by GMapping
-  double maxUrange_;
-  double maxrange_;
-  double sigma_;
-  int kernelSize_;
-  double lstep_;
-  double astep_;
-  int iterations_;
-  double lsigma_;
-  double ogain_;
-  int lskip_;
-  double srr_;
-  double srt_;
-  double str_;
-  double stt_;
-  double linearUpdate_;
-  double angularUpdate_;
-  double resampleThreshold_;
-  int particles_;
-  double xmin_;
-  double ymin_;
-  double xmax_;
-  double ymax_;
-  double delta_;
-  double llsamplerange_;
-  double llsamplestep_;
-  double lasamplerange_;
-  double lasamplestep_;
+    void laserCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+    
+    void updateMap(const sensor_msgs::LaserScan& scan);
+    bool getOdomPose(GMapping::OrientedPoint& gmap_pose, const ros::Time& t);
+    bool initMapper(const sensor_msgs::LaserScan& scan);
+    bool addScan(const sensor_msgs::LaserScan& scan, GMapping::OrientedPoint& gmap_pose);
+    double computePoseEntropy();
+    
+    // Parameters used by GMapping
+    double maxRange_;
+    double maxUrange_;
+    double maxrange_;
+    double sigma_;
+    int kernelSize_;
+    double lstep_;
+    double astep_;
+    int iterations_;
+    double lsigma_;
+    double ogain_;
+    int lskip_;
+    double srr_;
+    double srt_;
+    double str_;
+    double stt_;
+    double linearUpdate_;
+    double angularUpdate_;
+    double temporalUpdate_;
+    double resampleThreshold_;
+    int particles_;
+    double xmin_;
+    double ymin_;
+    double xmax_;
+    double ymax_;
+    double delta_;
+    double occ_thresh_;
+    double llsamplerange_;
+    double llsamplestep_;
+    double lasamplerange_;
+    double lasamplestep_;
 };
-
-}
-
-#endif /* BUILD_MAP_H_ */
